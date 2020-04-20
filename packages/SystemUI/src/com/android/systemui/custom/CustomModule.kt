946f3239741b036eca73928b2fd226fc64198cce
Linux4 <tim@linux4.de>
2020-04-20 17:57:43 +0200

    SystemUI: Add powershare QS tile
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: Ia09a674745adb433f3aaeebfeb79899906350793
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
@@ -6,6 +6,7 @@ package com.android.systemui.custom
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.CellularTile
+import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.WifiTile
 
 import dagger.Binds
@@ -26,4 +27,10 @@ interface CustomModule {
     @IntoMap
     @StringKey(CellularTile.TILE_SPEC)
     fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
+
+    /** Inject PowerShareTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(PowerShareTile.TILE_SPEC)
+    fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
 }
