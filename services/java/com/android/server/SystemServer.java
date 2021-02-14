2bc1786a64778a61624368a67b936059f04c1f07
Simão Gomes Viana <devel@superboring.dev>
2021-02-14 16:39:24 +0100

    SystemServer: add greeting
    Change-Id: I6a77d6342b4c61cb9445d34398ccea4c4a009447
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -828,6 +828,8 @@ public final class SystemServer implements Dumpable {
                         uptimeMillis);
             }
 
+            Slog.i("halogenOS", "Welcome to halogenOS! Let's eat some vanilla ice cream!");
+
             // In case the runtime switched since last boot (such as when
             // the old runtime was removed in an OTA), set the system
             // property so that it is in sync. We can't do this in
