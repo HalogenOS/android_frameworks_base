78a5848b3fbb5d2addf0e9c5522cbf54b1afa439
Simão Gomes Viana <devel@superboring.dev>
2023-03-19 14:03:14 +0100

    Revert "Screenshot: Unregister task listener on destroy"
    Google has released r35 as if it was an entirely new android version and
    completely effed us over with their millions of changes that could as
    well just have been saved up for AOSP 14.
    
    This reverts commit 57675cd2980a566c8f7be19a1bb01d7ed04a1bd0.

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -455,7 +455,6 @@ public class ScreenshotController {
         removeWindow();
         releaseMediaPlayer();
         releaseContext();
-        TaskStackChangeListeners.getInstance().unregisterTaskStackListener(mTaskListener);
         mBgExecutor.shutdownNow();
     }
 
