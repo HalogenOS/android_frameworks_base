d066d9db8c2363d6f3f8ce62500a763bf8d2fdc3
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-30 17:42:56 +0300

    exec spawning: support runtime resource overlays
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19500,4 +19500,10 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
         return token;
     }
+
+    @Override
+    public String[] getSystemIdmapPaths() {
+        // see comment in AssetManager#createSystemAssetsInZygoteLocked()
+        return android.content.res.AssetManager.systemIdmapPaths_;
+    }
 }
