df9cf9307f1adc4ea3298a070b06aa80b9f482f7
Michael Bestas <mkbestas@gmail.com>
2023-06-14 11:53:23 +0300

    SystemUI: Restore wifi and cellular QS tiles
    Change-Id: Ibbbabf30be08c6852f6aa968d28de94ac29b0e71
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
@@ -4,9 +4,26 @@
  */
 package com.android.systemui.custom
 
+import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.CellularTile
+import com.android.systemui.qs.tiles.WifiTile
+
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
+import dagger.multibindings.StringKey
 
 @Module
 interface CustomModule {
+    /** Inject WifiTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(WifiTile.TILE_SPEC)
+    fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
 
+    /** Inject CellularTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CellularTile.TILE_SPEC)
+    fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
 }
