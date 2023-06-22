b463ec0d358b69bfc15786e4d2a315f701f44cdc
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-06-22 11:18:26 +0300

    filter out non-system packages from location indicator exemptions
    Change-Id: I239c56745256fe18aeb0ef290d38e2ee355452e2

--- core/java/android/permission/PermissionManager.java
+++ core/java/android/permission/PermissionManager.java
@@ -79,6 +79,7 @@ import android.util.ArrayMap;
 import android.util.ArraySet;
 import android.util.DebugUtils;
 import android.util.Log;
+import android.util.PackageUtils;
 import android.util.Slog;
 
 import com.android.internal.R;
@@ -93,6 +94,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+
+import libcore.util.EmptyArray;
 
 /**
  * System level service for accessing the permission capabilities of the platform.
@@ -1393,6 +1396,11 @@ public final class PermissionManager {
             if (pkgName != null) {
                 pkgNames.add(pkgName);
             }
+        }
+        {
+            String[] arr = pkgNames.toArray(EmptyArray.STRING);
+            pkgNames.clear();
+            pkgNames.addAll(PackageUtils.filterNonSystemPackagesL(context, arr));
         }
         return pkgNames;
     }
