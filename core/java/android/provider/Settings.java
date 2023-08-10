0ca9ca106aeb494c4329a2ffc3e591d61206b6a0
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-08-10 18:29:09 +0300

    add RemoteKeyProvisioning server override setting

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14019,6 +14019,10 @@ public final class Settings {
         @Protected(restrictReads = false, readWrite = KnownSystemPackage.SETTINGS)
         public static final String WIDEVINE_PROVISIONING_SERVER = "widevine_provisioner_server";
 
+        /** @hide */
+        @Protected(restrictReads = false, readWrite = KnownSystemPackage.SETTINGS)
+        public static final String REMOTE_KEY_PROVISIONING_SERVER = "attest_remote_provisioner_server";
+
         // ExtSettings END
 
         // NOTE: If you add new settings here, be sure to add them to
