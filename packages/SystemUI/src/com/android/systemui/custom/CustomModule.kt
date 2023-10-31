215f2253002a3967e04d883e19bd0d0f6843b0b9
Simão Gomes Viana <devel@superboring.dev>
2023-11-01 00:19:02 +0100

    Add custom SystemUI module
    It's time to stop naming these things after the custom ROM and finally
    agree on a generic word like custom so that we can add changes from each
    other in harmony without polluting everything.
    
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

---
+++ packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2024 The halogenOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.android.systemui.custom
+
+import dagger.Module
+
+@Module
+interface CustomModule {
+
+}
