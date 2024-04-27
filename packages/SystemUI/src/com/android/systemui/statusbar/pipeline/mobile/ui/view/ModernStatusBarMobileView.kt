ef9b476e1814118edbc82b42e73f851bbcc7d6bf
lgjint <lgjint@protonmail.com>
2024-04-27 16:34:27 +0800

    SystemUI: Fix suspicious spaces around new mobile icons
    Referring to commit 7bb81fd27db87b00329b7cb3b7d95562700c4aa2, which addressed
    spacing issues around mobile icons. Reimplement this fix because this bug
    also appears in new mobile icons.
    
    Change-Id: I34843940337d19fa729425b705d3901d2434f708
    Signed-off-by: lgjint <lgjint@protonmail.com>
    Co-authored-by: LibXZR <i@xzr.moe>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/view/ModernStatusBarMobileView.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/view/ModernStatusBarMobileView.kt
@@ -44,6 +44,7 @@ class ModernStatusBarMobileView(context: Context, attrs: AttributeSet?) :
     ModernStatusBarView(context, attrs) {
 
     var subId: Int = -1
+    private lateinit var mobileGroup: AlphaOptimizedLinearLayout
 
     override fun toString(): String {
         return "ModernStatusBarMobileView(" +
@@ -52,6 +53,17 @@ class ModernStatusBarMobileView(context: Context, attrs: AttributeSet?) :
             "isCollecting=${binding.isCollecting()}, " +
             "visibleState=${getVisibleStateString(visibleState)}); " +
             "viewString=${super.toString()}"
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        mobileGroup = requireViewById(R.id.mobile_group)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mobileGroup.measure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(mobileGroup.measuredWidth, mobileGroup.measuredHeight)
     }
 
     public override fun onConfigurationChanged(newConfig: Configuration?) {
