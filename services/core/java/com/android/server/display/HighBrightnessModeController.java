halogenOS/android_frameworks_base_archived

Add support for HBM force-allow [1/2]

services/core/java/com/android/server/display/HighBrightnessModeController.java
@@ -86,6 +86,7 @@ class HighBrightnessModeController {
     private boolean mIsTimeAvailable = false;
     private boolean mIsAutoBrightnessEnabled = false;
     private boolean mIsAutoBrightnessOffByState = false;
+    private boolean mForceAllowHBM = false;
 
     // The following values are typically reported by DisplayPowerController.
     // This value includes brightness throttling effects.
@@ -358,10 +359,10 @@ class HighBrightnessModeController {
         // the MAX. HDR also needs to work under manual brightness which never adjusts the
         // brightness maximum; so we implement HDR-HBM in a way that doesn't adjust the max.
         // See {@link #getHdrBrightnessValue}.
-        return !mIsHdrLayerPresent
+        return mForceAllowHBM || ( !mIsHdrLayerPresent
                 && ((mIsAutoBrightnessEnabled && mIsTimeAvailable && mIsInAllowedAmbientRange)
                     || (!mIsAutoBrightnessEnabled && mHbmData.timeWindowMillis == 0))
-                && !mIsBlockedByLowPowerMode;
+                && !mIsBlockedByLowPowerMode );
     }
 
     private boolean deviceSupportsHbm() {
@@ -628,6 +629,8 @@ class HighBrightnessModeController {
     private final class SettingsObserver extends ContentObserver {
         private final Uri mLowPowerModeSetting = Settings.Global.getUriFor(
                 Settings.Global.LOW_POWER_MODE);
+        private final Uri mForceAllowHBMSetting = Settings.Global.getUriFor(
+                Settings.Global.FORCE_ALLOW_HBM);
         private boolean mStarted;
 
         SettingsObserver(Handler handler) {
@@ -636,15 +639,23 @@ class HighBrightnessModeController {
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            updateLowPower();
+            if (mLowPowerModeSetting.equals(uri)) {
+                updateLowPower();
+            }
+            if (mForceAllowHBMSetting.equals(uri)) {
+                updateForceAllowHBM();
+            }
         }
 
         void startObserving() {
             if (!mStarted) {
                 mContext.getContentResolver().registerContentObserver(mLowPowerModeSetting,
                         false /*notifyForDescendants*/, this, UserHandle.USER_ALL);
+                mContext.getContentResolver().registerContentObserver(mForceAllowHBMSetting,
+                        false /*notifyForDescendants*/, this, UserHandle.USER_ALL);
                 mStarted = true;
                 updateLowPower();
+                updateForceAllowHBM();
             }
         }
 
@@ -669,9 +680,19 @@ class HighBrightnessModeController {
             updateHbmMode();
         }
 
+        private void updateForceAllowHBM() {
+            mForceAllowHBM = isHBMForceAllowed();
+            updateHbmMode();
+        }
+
         private boolean isLowPowerMode() {
             return Settings.Global.getInt(
                     mContext.getContentResolver(), Settings.Global.LOW_POWER_MODE, 0) != 0;
+        }
+
+        private boolean isHBMForceAllowed() {
+            return Settings.Global.getInt(
+                    mContext.getContentResolver(), Settings.Global.FORCE_ALLOW_HBM, 0) != 0;
         }
     }
 

HalogenOS/android_frameworks_base

Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

services/core/java/com/android/server/display/HighBrightnessModeController.java
@@ -91,6 +91,7 @@ class HighBrightnessModeController {
     private boolean mIsTimeAvailable = false;
     private boolean mIsAutoBrightnessEnabled = false;
     private boolean mIsAutoBrightnessOffByState = false;
+    private boolean mForceAllowHBM = false;
 
     // The following values are typically reported by DisplayPowerController.
     // This value includes brightness throttling effects.
@@ -364,9 +365,9 @@ class HighBrightnessModeController {
         // the MAX. HDR also needs to work under manual brightness which never adjusts the
         // brightness maximum; so we implement HDR-HBM in a way that doesn't adjust the max.
         // See {@link #getHdrBrightnessValue}.
-        return !mIsHdrLayerPresent
+        return mForceAllowHDM || ( !mIsHdrLayerPresent
                 && (mIsAutoBrightnessEnabled && mIsTimeAvailable && mIsInAllowedAmbientRange
-                && !mIsBlockedByLowPowerMode);
+                && !mIsBlockedByLowPowerMode) );
     }
 
     boolean deviceSupportsHbm() {
@@ -640,6 +641,8 @@ class HighBrightnessModeController {
     private final class SettingsObserver extends ContentObserver {
         private final Uri mLowPowerModeSetting = Settings.Global.getUriFor(
                 Settings.Global.LOW_POWER_MODE);
+        private final Uri mForceAllowHBMSetting = Settings.Global.getUriFor(
+                Settings.Global.FORCE_ALLOW_HBM);
         private boolean mStarted;
 
         SettingsObserver(Handler handler) {
@@ -648,15 +651,23 @@ class HighBrightnessModeController {
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            updateLowPower();
+            if (mLowPowerModeSetting.equals(uri)) {
+                updateLowPower();
+            }
+            if (mForceAllowHBMSetting.equals(uri)) {
+                updateForceAllowHBM();
+            }
         }
 
         void startObserving() {
             if (!mStarted) {
                 mContext.getContentResolver().registerContentObserver(mLowPowerModeSetting,
                         false /*notifyForDescendants*/, this, UserHandle.USER_ALL);
+                mContext.getContentResolver().registerContentObserver(mForceAllowHBMSetting,
+                        false /*notifyForDescendants*/, this, UserHandle.USER_ALL);
                 mStarted = true;
                 updateLowPower();
+                updateForceAllowHBM();
             }
         }
 
@@ -681,9 +692,19 @@ class HighBrightnessModeController {
             updateHbmMode();
         }
 
+        private void updateForceAllowHBM() {
+            mForceAllowHBM = isHBMForceAllowed();
+            updateHbmMode();
+        }
+
         private boolean isLowPowerMode() {
             return Settings.Global.getInt(
                     mContext.getContentResolver(), Settings.Global.LOW_POWER_MODE, 0) != 0;
+        }
+
+        private boolean isHBMForceAllowed() {
+            return Settings.Global.getInt(
+                    mContext.getContentResolver(), Settings.Global.FORCE_ALLOW_HBM, 0) != 0;
         }
     }
 