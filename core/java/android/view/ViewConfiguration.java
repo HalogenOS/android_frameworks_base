aa780c9143e65b1ab14345c49a05e55c41076d58
Simão Gomes Viana <devel@superboring.dev>
2024-10-05 18:12:36 +0000

    Revert "ViewConfiguration: add support for max/min fling velocity sysproperties"
    Let's use the resource configs in device overlays instead
    
    This reverts commit 5e344f30d2a7bd38d59d4d2d53ff139bbea580e9.

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -33,7 +33,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.StrictMode;
-import android.os.SystemProperties;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.SparseArray;
@@ -532,47 +531,10 @@ public class ViewConfiguration {
         mHandwritingGestureLineMargin = res.getDimensionPixelSize(
                 com.android.internal.R.dimen.config_viewConfigurationHandwritingGestureLineMargin);
 
-        String minFlingVeloProp = "ro.min.fling_velocity"; // Min fling prop
-        String maxFlingVeloProp = "ro.max.fling_velocity"; // Max fling prop
-
-        // Get the properties
-        String minFlingVeloSysProp = SystemProperties.get(minFlingVeloProp);
-        String maxFlingVeloSysProp = SystemProperties.get(maxFlingVeloProp);
-        boolean isMaxFlingVeloPredefined = false;
-        boolean isMinFlingVeloPredefined = false;
-        int minFlingVeloTmp = 0;
-        int maxFlingVeloTmp = 0;
-
-        // Check whether the property values are valid
-        if(minFlingVeloSysProp != null && (!minFlingVeloSysProp.isEmpty()) &&
-            isNumeric(minFlingVeloSysProp)) {
-            minFlingVeloTmp = Integer.parseInt(minFlingVeloSysProp);
-            isMinFlingVeloPredefined = true;
-        }
-
-        if(maxFlingVeloSysProp != null && (!maxFlingVeloSysProp.isEmpty()) &&
-            isNumeric(maxFlingVeloSysProp)) {
-            maxFlingVeloTmp = Integer.parseInt(maxFlingVeloSysProp);
-            isMaxFlingVeloPredefined = true;
-        }
-
-        // Use config values if no prop available or invalid
-        if(!isMinFlingVeloPredefined && minFlingVeloTmp == 0)
-            minFlingVeloTmp = res.getDimensionPixelSize(
-                    com.android.internal.R.dimen.config_viewMinFlingVelocity);
-        if(!isMaxFlingVeloPredefined && maxFlingVeloTmp == 0)
-            maxFlingVeloTmp = res.getDimensionPixelSize(
-                    com.android.internal.R.dimen.config_viewMaxFlingVelocity);
-
-        // Check again for availability, otherwise use default values
-        if(minFlingVeloTmp * maxFlingVeloTmp == 0) {
-            minFlingVeloTmp = MINIMUM_FLING_VELOCITY;
-            maxFlingVeloTmp = MAXIMUM_FLING_VELOCITY;
-        }
-
-        // Assign the final variables
-        mMinimumFlingVelocity = minFlingVeloTmp;
-        mMaximumFlingVelocity = maxFlingVeloTmp;
+        mMinimumFlingVelocity = res.getDimensionPixelSize(
+                com.android.internal.R.dimen.config_viewMinFlingVelocity);
+        mMaximumFlingVelocity = res.getDimensionPixelSize(
+                com.android.internal.R.dimen.config_viewMaxFlingVelocity);
 
         int configMinRotaryEncoderFlingVelocity = res.getDimensionPixelSize(
                 com.android.internal.R.dimen.config_viewMinRotaryEncoderFlingVelocity);
@@ -626,16 +588,6 @@ public class ViewConfiguration {
         mViewBasedRotaryEncoderScrollHapticsEnabledConfig =
                 res.getBoolean(
                         com.android.internal.R.bool.config_viewBasedRotaryEncoderHapticsEnabled);
-    }
-
-    /** @hide */
-    private static boolean isNumeric(String string) {
-        try {
-            Integer.parseInt(string);
-        } catch(NumberFormatException e) {
-            return false;
-        }
-        return true;
     }
 
     /**
