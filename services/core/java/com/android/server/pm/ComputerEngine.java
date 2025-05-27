19d4400a9f1b9b5976931cfc0ef75e6d39588a87
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-05-27 15:46:25 +0000

    Hide provider(s) for startup on apps with valid Play Store source stamp

--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -4796,6 +4796,9 @@ public class ComputerEngine implements Computer {
         }
         final PackageStateInternal packageState = getPackageStateInternal(
                 providerInfo.packageName);
+        if (com.android.server.pm.ext.PackageHooks.shouldHideContentProvider(packageState, providerInfo)) {
+            return null;
+        }
         if (!PackageStateUtils.isEnabledAndMatches(packageState, providerInfo, flags, userId)) {
             return null;
         }
@@ -4884,6 +4887,9 @@ public class ComputerEngine implements Computer {
                 continue;
             }
             final PackageStateInternal ps = mSettings.getPackage(providerInfo.packageName);
+            if (com.android.server.pm.ext.PackageHooks.shouldHideContentProvider(ps, providerInfo)) {
+                continue;
+            }
             final ComponentName component =
                     new ComponentName(providerInfo.packageName, providerInfo.name);
             if (shouldFilterApplication(
