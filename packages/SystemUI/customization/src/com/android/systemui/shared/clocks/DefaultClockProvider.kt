1aba5ce93b7a13f2a67082ba0ab309cc650116c9
Simão Gomes Viana <devel@superboring.dev>
2025-07-13 22:50:22 +0200

    Use config_clockFontFamily for default clock typeface
    Fixes: b/364680873

--- packages/SystemUI/customization/src/com/android/systemui/shared/clocks/DefaultClockProvider.kt
+++ packages/SystemUI/customization/src/com/android/systemui/shared/clocks/DefaultClockProvider.kt
@@ -85,7 +85,9 @@ class DefaultClockProvider(
             val clockSettings = settings.copy(axes = ClockAxisStyle(fontAxes))
             val typefaceCache =
                 TypefaceCache(buffers.infraMessageBuffer, NUM_CLOCK_FONT_ANIMATION_STEPS) {
-                    FLEX_TYPEFACE
+                    Typeface.create(resources.getString(
+                        com.android.internal.R.string.config_clockFontFamily
+                    ), Typeface.NORMAL)
                 }
             FlexClockController(
                 ClockContext(
@@ -143,10 +145,5 @@ class DefaultClockProvider(
         // 750ms @ 120hz -> 90 frames of animation
         // In practice, 30 looks good enough and limits our memory usage
         const val NUM_CLOCK_FONT_ANIMATION_STEPS = 30
-
-        val FLEX_TYPEFACE by lazy {
-            // TODO(b/364680873): Move constant to config_clockFontFamily when shipping
-            Typeface.create("google-sans-flex-clock", Typeface.NORMAL)
-        }
     }
 }
