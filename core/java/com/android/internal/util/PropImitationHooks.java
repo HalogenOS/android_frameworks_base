ebc984842518b59a87ff69a65f77fb22f375cf8d
Simão Gomes Viana <devel@superboring.dev>
2025-08-13 21:29:18 +0200

    PropImitationHooks: change logging on shouldBypassTaskPermission

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -397,7 +397,7 @@ public class PropImitationHooks {
             gmsUid = context.getPackageManager().getApplicationInfo(PACKAGE_GMS, 0).uid;
             dlog("shouldBypassTaskPermission: gmsUid:" + gmsUid + " callingUid:" + callingUid);
         } catch (Exception e) {
-            Log.e(TAG, "shouldBypassTaskPermission: unable to get gms uid", e);
+            Log.d(TAG, "shouldBypassTaskPermission: unable to get gms uid", e);
             return false;
         }
         return gmsUid == callingUid;
