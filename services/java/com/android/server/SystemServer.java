6b0c15086bb5c0258bee6eff138413909850aeac
Simão Gomes Viana <devel@superboring.dev>
2026-01-12 23:41:30 +0100

    fixup! SystemServer: add greeting

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -898,7 +898,7 @@ public final class SystemServer implements Dumpable {
                         uptimeMillis);
             }
 
-            Slog.i("halogenOS", "Welcome to halogenOS! Sweet, sweet Baklava for everyone!");
+            Slog.i("halogenOS", "Welcome to halogenOS! Extra sweet Baklava for everyone!");
 
             // In case the runtime switched since last boot (such as when
             // the old runtime was removed in an OTA), set the system
